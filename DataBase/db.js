8861f5b956ad33b93bfa51c3edad785f75d5e5b9
danielaq <daniela.quijano@ucr.ac.cr>
2024-07-05 17:12:35 -0600

    Second commit

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('Proyecto', 'prueba', '12345678', {
-  host: 'LAPTOP-NO632R4G',
+const db = new Sequelize('Proyecto', 'prueba', '12345', {
+  host: 'BIBLIO-404787',
   dialect: 'mssql',
   logging: false, // Desactiva el registro de consultas SQL en la consola (opcional)
 });
