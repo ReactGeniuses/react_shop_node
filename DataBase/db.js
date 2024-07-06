0e469c2b371737608ab428651de569b08989810f
danielaq <daniela.quijano@ucr.ac.cr>
2024-07-05 18:31:30 -0600

    cloud database

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,7 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('Proyecto', 'prueba', '12345', {
+/* const db = new Sequelize('Proyecto', 'prueba', '12345', {
   host: 'BIBLIO-404787',
+  dialect: 'mssql',
+  logging: false, // Desactiva el registro de consultas SQL en la consola (opcional)
+}); */
+
+const db = new Sequelize('shopshop', 'shopshop', 'DQ@20241a', {
+  host: 'eu-az-sql-serv1.database.windows.net',
   dialect: 'mssql',
   logging: false, // Desactiva el registro de consultas SQL en la consola (opcional)
 });
