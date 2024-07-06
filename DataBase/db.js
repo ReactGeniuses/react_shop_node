8282db2061b8a4feb69e1fd892abfadad586da06
danielaq <daniela.quijano@ucr.ac.cr>
2024-07-05 18:55:39 -0600

    database commit

--- DataBase/db.js
+++ DataBase/db.js
@@ -9,7 +9,7 @@ import { Sequelize } from 'sequelize';
 const db = new Sequelize('shopshop', 'shopshop', 'DQ@20241a', {
   host: 'eu-az-sql-serv1.database.windows.net',
   dialect: 'mssql',
-  logging: false, // Desactiva el registro de consultas SQL en la consola (opcional)
+  logging: false, ////Desactiva el registro de consultas SQL en la consola (opcional)
 });
 
 export default db;
