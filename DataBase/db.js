14ed55d325b4530d33c7482c01b1c12644522a7b
danielaq <daniela.quijano@ucr.ac.cr>
2024-07-05 21:25:18 -0600

    db

--- DataBase/db.js
+++ DataBase/db.js
@@ -8,14 +8,13 @@ import { Sequelize } from 'sequelize';
 
 const db = new Sequelize({
   dialect: 'mssql',
-  host: 'eu-az-sql-serv1.database.windows.net',
-  database: 'shopshop',
-  username: 'shopshop',
-  password: 'DQ@20241a',
-  options: {
-    encrypt: true, // Usar cifrado para la conexión
-    trustServerCertificate: false, // No confiar en el certificado del servidor
+  dialectOptions: {
+    options: {
+      encrypt: true,
+      trustServerCertificate: false,
+    },
   },
+  connectionString: 'Server=tcp:eu-az-sql-serv1.database.windows.net,1433;Initial Catalog=shopshop;Persist Security Info=False;User ID=shopshop;Password=DQ@20241a;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;',
   logging: false, // Desactiva el registro de consultas SQL en la consola (opcional)
 });
 
