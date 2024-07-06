4fcdfe441cd571f9055d743d9442ca9c4ba4fdeb
danielaq <daniela.quijano@ucr.ac.cr>
2024-07-05 21:39:01 -0600

    db

--- DataBase/db.js
+++ DataBase/db.js
@@ -6,20 +6,10 @@ import { Sequelize } from 'sequelize';
   logging: false, // Desactiva el registro de consultas SQL en la consola (opcional)
 }); */
 
-const db = new Sequelize({
-  dialect: 'mssql',
-  dialectOptions: {
-    options: {
-      encrypt: true,
-      trustServerCertificate: false,
-    },
-  },
+const db = new Sequelize('shopshop', 'shopshop', 'DQ@20241a', {
   host: 'eu-az-sql-serv1.database.windows.net',
-  database: 'shopshop',
-  username: 'shopshop',
-  password: 'DQ@20241a',
-  port: process.env.PORT || 1433,
-  logging: false,
+  dialect: 'mssql',
+  logging: false, ////Desactiva el registro de consultas SQL en la consola (opcional)
 });
 
 export default db;
